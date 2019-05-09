e5038bd41f28fc1ad5ddd1e4793928f00d161150
sahjoo <joosangah@gmail.com>
2019-05-08 20:44:47 -0400

    checkpoint 10 complete

---
+++ scripts/player-bar.js
@@ -0,0 +1,15 @@
+{
+  $('button#play-pause').on('click', function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') { return; } // terminates if song isn't playing
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
+    const nextSong = album.songs[nextSongIndex]; // defines nextSong
+    player.playPause(nextSong);
+  });
+}
