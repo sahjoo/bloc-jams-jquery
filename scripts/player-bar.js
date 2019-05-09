241ee29b4937946b69230ae6be87c02c62373b83
sahjoo <joosangah@gmail.com>
2019-05-08 23:53:43 -0400

    assignment 11 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -32,6 +32,11 @@
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
     $('#time-control .current-time').text( currentTime);
+    $('#time-control .total-time').text( duration);
     $('#time-control input').val(percent);
   }, 1000);
+
+  $('#volume-control input').on('input', function(event) {
+    player.setVolume(event.target.value);
+  });
 }
