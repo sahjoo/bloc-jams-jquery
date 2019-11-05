8dbcc647b8bc7939199cb01d3996724514d5ead4
sahjoo <joosangah@gmail.com>
2019-11-05 18:14:47 -0500

    correcting the missing . in front of .text

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,7 +2,7 @@ class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
     const totalTime = player.currentlyPlaying.duration;
-    $('#time-control .total-time')text(player.prettyTime(totalTime));
+    $('#time-control .total-time').text(player.prettyTime(totalTime));
   }
 }
 
